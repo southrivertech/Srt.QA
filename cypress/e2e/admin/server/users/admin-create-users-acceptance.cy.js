4161b27b319e251d2075e152497573c54d49f33a
Sukhpreet Singh <45624656+singh-sukhpreet@users.noreply.github.com>
2023-08-31 18:27:50 -0400

    addition of user creation test case

---
+++ cypress/e2e/admin/server/users/admin-create-users-acceptance.cy.js
@@ -0,0 +1,63 @@
+import navigationSelectors from '../../../../../selectors/navigation/left-navigation-selectors.json'
+import userSelectors from '../../../../../selectors/user/user-selectors.json'
+/**
+ * @description
+ * This spec file contains test to verify that admin user can create users for an existing server
+ *
+ * @file
+ * cypress/e2e/admin/server/users/admin-create-users-acceptance.cy.js
+ *
+ * @breadcrumb
+ * Login > {existing server} > users
+ *
+ * @assertions
+ * To verify that admin can create users
+ *
+ *  @prerequisites
+ * Pre-Requisite data:
+ * - user should have valid credentials
+ */
+
+describe('Login > {existing server} > users', () => {
+  const adminData = Cypress.env('admin')
+  const userInfo = {
+    username: adminData.adminUsername,
+    password: adminData.adminPassword
+  }
+  const lookForText = {
+    homeUrlText: '/Console'
+  }
+
+  const userDetails = {
+    userName: `qa-auto server ${Cypress.dayjs().format('ssmmhhMMYY')}`,
+    password: 'testing123'
+  }
+
+  beforeEach('login', () => {
+    cy.postApiLogin()
+    cy.waitForNetworkIdlePrepare({
+      method: 'POST',
+      pattern: '**WebApi/Login**',
+      alias: 'postApiLogin',
+      log: false
+    })
+    cy.login(adminData.adminBaseUrl, userInfo.username, userInfo.password)
+    cy.waitForNetworkIdle('@postApiLogin', 500).its('callCount').should('equal', 1)
+    cy.url().should('include', lookForText.homeUrlText)
+    cy.waitApiResponseStatusCode('@postApiLogin', 200)
+  })
+
+  it('verify that admin can create users', () => {
+    cy.get(navigationSelectors.textLabelSelector).contains('ws01').click()
+    cy.get(navigationSelectors.textLabelSelector).contains('qa auto DO NOT DELETE').should('be.visible').click()
+    cy.get(navigationSelectors.textLabelSelector).contains('Users').should('be.visible').click()
+    cy.get(userSelectors.addButton).should('be.visible').click()
+    cy.createUser(userDetails)
+    // cy.get(serverSelectors.serverName).contains(serverDetails.serverName).should('be.visible')
+  })
+
+  afterEach('deleting a user', () => {
+    // cy.deleteServer(serverDetails.serverName)
+    // cy.get(serverSelectors.serverName).contains(serverDetails.serverName).should('not.exist')
+  })
+})
