22c333b6fdf89ef83ffbe87015f0eef5b82045e3
singh-sukhpreet <sukhi.singh4424@gmail.com>
2024-03-22 09:36:25 -0600

    created spec files and command files for getServerIPBansList and getServerVariables

---
+++ components/apis/requests/domain/server/get-server-ipBan-list-api-request.js
@@ -0,0 +1,28 @@
+/**
+* @description
+* The getIPBanListApiRequest command is used to get the list of banned IPs
+*
+* @parameters
+* @param {required} bearerToken
+*
+* @example
+* cy.getIPBanListApiRequest(bearerToken)
+*/
+
+Cypress.Commands.add('getIPBanListApiRequest', (serverDetails) => {
+  Cypress.log({
+    name: 'getIPBanListApiRequest'
+  })
+
+  cy.api({
+    method: 'GET',
+    url: `${Cypress.env('apiBaseUrl')}/api/IPBans/${serverDetails.serverName}`,
+    headers: {
+      Authorization: `Bearer ${serverDetails.bearerToken}`
+    }
+  }).then(($response) => {
+    console.log('response of getIPBanListApiRequest', $response)
+    expect($response.status).to.eq(200)
+    return $response.body
+  })
+})
